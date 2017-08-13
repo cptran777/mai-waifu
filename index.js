340d661a6dc3ee62a179e119c76da3065584ffde
cptran777 <cptran14@gmail.com>
2017-08-12 20:22:01 -0700

    initial node server stuff

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/******************* INIT DEPENDENCIES *********************/
+
+const express = require('express');
+const bodyparser = require('body-parser');
+const path = require('path');
+const request = require('request');
+
+const app = express();
+
+/***************** INIT CUSTOM MODULES *********************/
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyparser.json());
+
+/******************** START  ROUTES ************************/
+
+app.get('/', (request, response) => {
+  response.json({ success: 'true' });
+});
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log('Listening on port ', port);
+});
